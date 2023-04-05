8d27b4855f391f8db232034ffaa499a8266aedcd
John Spackman <john.spackman@zenesis.com>
2023-04-05 13:16:15 +0100

    license header

--- source/class/qxl/datagrid/column/FilteredColumns.js
+++ source/class/qxl/datagrid/column/FilteredColumns.js
@@ -1,3 +1,24 @@
+/* ************************************************************************
+*
+*    Qooxdoo DataGrid
+*
+*    https://github.com/qooxdoo/qooxdoo
+*
+*    Copyright:
+*      2022-23 Zenesis Limited, https://www.zenesis.com
+*
+*    License:
+*      MIT: https://opensource.org/licenses/MIT
+*
+*      This software is provided under the same licensing terms as Qooxdoo,
+*      please see the LICENSE file in the Qooxdoo project's top-level directory
+*      for details.
+*
+*    Authors:
+*      * John Spackman (john.spackman@zenesis.com, @johnspackman)
+*
+* *********************************************************************** */
+
 /**
  * Wraps another instance of IColumns to filter and sort the list; this allows the user interface
  * components to be given a subset to work on, and not worry about the start index or the ordering
