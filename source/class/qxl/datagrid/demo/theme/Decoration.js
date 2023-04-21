cd28c8aa8a2f1bf9b0b8ed593c2493c4c560d743
John Spackman <john.spackman@zenesis.com>
2023-04-21 09:59:34 +0100

    license header

--- source/class/qxl/datagrid/demo/theme/Decoration.js
+++ source/class/qxl/datagrid/demo/theme/Decoration.js
@@ -1,3 +1,24 @@
+/* ************************************************************************
+*
+*    Qooxdoo DataGrid
+*
+*    https://github.com/qooxdoo/qooxdoo
+*
+*    Copyright:
+*      2022-23 Zenesis Limited, https://www.zenesis.com
+*
+*    License:
+*      MIT: https://opensource.org/licenses/MIT
+*
+*      This software is provided under the same licensing terms as Qooxdoo,
+*      please see the LICENSE file in the Qooxdoo project's top-level directory
+*      for details.
+*
+*    Authors:
+*      * John Spackman (john.spackman@zenesis.com, @johnspackman)
+*
+* *********************************************************************** */
+
 qx.Theme.define("qxl.datagrid.demo.theme.Decoration", {
   extend: qx.theme.tangible.Decoration,
   include: [qxl.datagrid.theme.MDecoration]
