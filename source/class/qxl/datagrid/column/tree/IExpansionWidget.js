548f56dfe1b03e7078fd556617de849fa5af24a5
WillsterJohnson <wjohnson@vmn.digital>
2023-08-14 13:46:24 +0100

    :memo: Add copyright notice

--- source/class/qxl/datagrid/column/tree/IExpansionWidget.js
+++ source/class/qxl/datagrid/column/tree/IExpansionWidget.js
@@ -1,4 +1,26 @@
-qx.Interface.define("qxl.datagrid.column.tree.IExpansionWidget", {
+/* ************************************************************************
+ *
+ *    Qooxdoo DataGrid
+ *
+ *    https://github.com/qooxdoo/qooxdoo
+ *
+ *    Copyright:
+ *      2022-23 Zenesis Limited, https://www.zenesis.com
+ *
+ *    License:
+ *      MIT: https://opensource.org/licenses/MIT
+ *
+ *      This software is provided under the same licensing terms as Qooxdoo,
+ *      please see the LICENSE file in the Qooxdoo project's top-level directory
+ *      for details.
+ *
+ *    Authors:
+ *      * John Spackman (john.spackman@zenesis.com, @johnspackman)
+ *      * Will Johnson (willsterjohnson)
+ *
+ * *********************************************************************** */
+
+qx.Interface.define( "qxl.datagrid.column.tree.IExpansionWidget", {
   properties: {
     /** How deep the indentation level is */
     indentationLevel: {
